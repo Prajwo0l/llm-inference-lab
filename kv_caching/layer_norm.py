f7f82c827aaeb475a63e725db867dd01bbc7c1c8
Prajwol <lamichhaneprajwool@gmail.com>
2026-04-22 08:58:21 +0545

    Fixed some bugs

--- kv_caching/layer_norm.py
+++ kv_caching/layer_norm.py
@@ -1,21 +1,20 @@
-import torch 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class LayerNorm(nn.Module):
-    """
-    Layer Norm with Optional bias
-    args:
-    ndim:Number of features(last dimension of input)
-    bias:If true,learns an additive bias 
-    """
-    def __init__(self,ndim:int,bias:bool=True):
-        super().__init__()
-        self.weight=nn.Parameter(torch.ones(ndim))
-        self.bias=nn.Parameter(torch.zeros(ndim)) if bias else None
+    """LayerNorm with optional bias.
 
-    def forward(self,x:torch.Tensor)-> torch.Tensor:
-        return F.layer_norm(x,self.weight.shape,self.weight,self.bias,1e-5)
-    
-    
+    Args:
+        ndim (int): Number of features (last dimension of input).
+        bias (bool): If True, learns an additive bias. Default: True.
+    """
+
+    def __init__(self, ndim: int, bias: bool = True):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(ndim))
+        self.bias   = nn.Parameter(torch.zeros(ndim)) if bias else None
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return F.layer_norm(x, self.weight.shape, self.weight, self.bias, 1e-5)
